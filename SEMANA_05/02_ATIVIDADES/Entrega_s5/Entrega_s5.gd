33b6be2da805bf1d88f8c496a1cdcbf1fa9f61c7
FelipeSSpina <felipe.spina@sou.inteli.edu.br>
2023-03-03 08:39:09 -0300

    Adicionei mais alguns comentários

--- SEMANA_05/02_ATIVIDADES/Entrega_s5/Entrega_s5.gd
+++ SEMANA_05/02_ATIVIDADES/Entrega_s5/Entrega_s5.gd
@@ -9,7 +9,7 @@ var nome = '' #Nessa linha adicionei a variavel "nome", que será usada em algum
 func _on_Button_pressed(): #Função relacionada com o primeiro botão
 	#Coletando dados inseridos pelo usuário
 	numero = int($LineEdit.text) #Nessa linha alterei a variável que estava "número" (com acento) para "numero" (sem acento) e adicionei o sinal "$" antes de LineEdit
-	$Label4.text = "Sua resposta é: " + str(numero)
+	$Label4.text = "Sua resposta é: " + str(numero) #Nessa linha mostra somente o número digitado pelo usuário na tela
 
 func _on_Button2_pressed(): #Função relacionada com o segundo botão
 	#Incrementando o número inserido pelo usuário
@@ -17,13 +17,13 @@ func _on_Button2_pressed(): #Função relacionada com o segundo botão
 	for i in range(10):
 		numero+=i #Nessa linha alterei a variável qu estava "Numero" (com letra maiúscula) para "numero
 		lista.append(numero) #Nessa linha alterei a variável qu estava "Numero" (com letra maiúscula) para "numero
-	$Label2.text = "O número somado é: " + str(numero)
+	$Label2.text = "O número somado é: " + str(numero) #Nessa linha mostra o número que foi somado na tela
 
 
 func _on_Button3_pressed(): #Função relacionada com o terceiro botão
-	nome = $LineEdit2.text #Aqui eu coloco na variável "nome" o que for inserido pelo usuário
 	#Mudando o nome do usuário de acordo com os dados da lista
 	#Se houver algum número ímpar o nome deve adicionar "baldo" ao final
+	nome = $LineEdit2.text #Aqui eu coloco na variável "nome" o que for inserido pelo usuário
 	var cont = 0 #Adicionei nessa linha a variável "cont" que foi colocada dentro do while sem definir (sem o "var")
 	for i in (len(lista)): #Criei um for baseado no tamanho da lista, para verifcar todos os valroes dela
 		if(lista[i] % 2 == 1): #Aqui irá verificar se o número é ímpar
@@ -31,5 +31,5 @@ func _on_Button3_pressed(): #Função relacionada com o terceiro botão
 		if(cont!=0): #se for diferente de 0 será adicionado baldo ao final do nome
 			nome = nome+"baldo" #adiciona baldo no final
 			break #Esse break faz com que o looping do for pare, evitando que baldo seja adicionado diversas vezes no nome
-	$Label5.text = nome
+	$Label5.text = nome #Nessa linha mostra o nome do usuário na tela
 	
